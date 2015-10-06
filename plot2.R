92ed7015d6f0a6bb5b1bfa3507b3121fb770d3cd
chiggs01 <chiggs@no_reply.edu.au>
2015-10-06 12:14:39 +1100

    Plot 2 saved to disk

--- plot2.R
+++ plot2.R
@@ -1,26 +1,34 @@
 ## This script downloads the UCI Individual household electric power 
-## consumption Data Set and plots ...
+## consumption Data Set and plots Global Active Power over time.
 
 plot2 <- function() {
 
-# Set constants
+    # Set constants
     fileName="household_power_consumption.txt"
     fileURL="https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
     
-# Download and uncompress data from Internet 
-if(!file.exists(fileName)) {
-    myTemp <- tempfile()
-    download.file(fileURL, myTemp)
-    unzip(myTemp, overwrite = TRUE)
-    unlink(myTemp)
+    # Download and uncompress data from Internet 
+    if(!file.exists(fileName)) {
+        myTemp <- tempfile()
+        download.file(fileURL, myTemp)
+        unzip(myTemp, overwrite = TRUE)
+        unlink(myTemp)
     }
-
-#   Identify column classes and load data
-    myClasses <- sapply(read.table(fileName, nrows=1, sep=";", header=TRUE), class)     
-    myData<-read.table(fileName, na.strings="?", header = TRUE, sep = ";", colClasses = myClasses, nrows = 10)
-#    myRows<-myData$Date=="1/2/2007"|myData$Date=="2/2/2007"
-#    myData<-myData[myRows,]
-    myData$Date<-as.Date(myData$Date, "%d/%m/%Y")    
-
-myData    
+    
+    # Identify column classes and load data
+    myClasses <- sapply(read.table(fileName, nrows=1, sep=";", header=TRUE),
+                        class)
+    myData<-read.table(fileName, na.strings="?", header = TRUE, sep = ";", 
+                       colClasses = myClasses)
+    myRows<-myData$Date=="1/2/2007"|myData$Date=="2/2/2007"
+    myData<-myData[myRows,]
+    
+    #Add column for converted date/time
+    myData$DateTime<-strptime(paste(myData$Date,myData$Time),"%d/%m/%Y %H:%M:%S")
+    
+    #display chart and save to file
+    plot(myData$DateTime, myData$Global_active_power, type="l",
+         ylab="Global Active Power (kilowatts)", xlab="")
+    dev.copy(png, file="plot2.png", width = 480, height = 480)
+    dev.off()    
 }
